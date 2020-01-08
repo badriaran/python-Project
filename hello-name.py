543c5a4ef8c2fcc9a9cb8a8ec497037d05e8e5dd
Badri Aran <badriaran10@gmail.com>
2020-01-08 11:40:29 +0545

    day is aweesome.

---
+++ hello-name.py
@@ -0,0 +1,17 @@
+first= "hello"
+last= "badri"
+full=  first + last
+print(full)
+full=first +" " +last
+print(full)
+print(full.title())
+print(full.upper())
+print(full.lower())
+
+greet="hello"
+#name="badri"
+name = input("ypur name:")
+greeting= f"{greet}, {name}!"
+greeting2="{}.{}!".format(greet, name)
+print(greeting2)
+#print(greeting)
---
+++ hello-world.py
@@ -0,0 +1,6 @@
+print("hello world")
+message="hello python"
+print('message')
+name="badri don"
+print(name.title())
+print(name.upper())
